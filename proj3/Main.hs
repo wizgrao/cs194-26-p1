30b6f8d5ed9d4e1879664e339e29d0cf373129b7
Gaurav Rao <gauravity@berkeley.edu>
2020-03-05 19:31:53 -0800

    finito part 2

--- proj3/Main.hs
+++ proj3/Main.hs
@@ -6,13 +6,27 @@ import System.Environment
 import Graphics.Image as I
 import Debug.Trace
 import Data.List as L
+import Text.CSV
+import Data.Either
+import Control.Monad
 
 main :: IO ()
 main = 
   do args <- getArgs
-     img  <- readImg $ head args
+     img  <- readImg $ head args :: IO Im
+     img2  <- readImg $ args !! 1 :: IO  Im
      displayImage img
-     displayImage $ testRaster img
-     getLine >> return ()
+     displayImage img2  
+     eitherCSV1 <- parseCSVFromFile $ args !! 2
+     print eitherCSV1
+     let csv1 = cheeseError eitherCSV1
+     eitherCSV2 <- parseCSVFromFile $ args !! 3
+     let csv2 = cheeseError eitherCSV2
+     if null csv1 || null csv2 then putStrLn "Error Loading CSV" else putStrLn "csvs loaded!"
+     let tringle1 = csvToTriangles csv1
+     let tringle2 = csvToTriangles csv2
+     displayImage $ warpTriangles img tringle1 tringle2
+     if args !! 4 /= "yuh" then mapM_ (\i -> writeImage (args !! 4 ++ show i ++ ".png") (interpImages (1.0/45.0*fromIntegral i) img tringle1 img2 tringle2) >> putStrLn ("Wrote " ++ show i)) [0 .. 44] else return ()
+     void getLine  
 
 
--- src/P3.hs
+++ src/P3.hs
@@ -1,13 +1,24 @@
 module P3
   ( computeAffine 
-  , rasterImage
-  , testRaster
+  , csvToTriangles
+  , interpImages 
+  , cheeseError
+  , Im
+  , warpTriangles
   ) where
+
 
 import qualified Numeric.LinearAlgebra.Static as S
 import GHC.TypeNats as T
 import qualified Numeric.LinearAlgebra as L
 import qualified Graphics.Image as I
+import Data.Either
+import Text.CSV
+import Text.Parsec.Error
+
+cheeseError :: Either ParseError CSV -> CSV
+cheeseError (Left _) = []
+cheeseError (Right csv) = csv
 
 computeAffine :: Triangle -> Triangle -> S.Sq 3  
 computeAffine (Triangle x1 x2 x3) (Triangle y1 y2 y3) = 
@@ -24,8 +35,7 @@ toLin :: S.R 3 -> S.R 2
 toLin x = let (y, _) = S.split x in y
 
 inTriangle :: Triangle -> S.R 2 -> Bool
-inTriangle (Triangle x1 x2 x3) x = halfSpace x1 x2 x >= 0 && halfSpace x2 x3 x >= 0 && halfSpace x3 x1 x >=0
-
+inTriangle (Triangle x1 x2 x3) x = (halfSpace x1 x2 x >= 0 && halfSpace x2 x3 x >= 0 && halfSpace x3 x1 x >=0) || (halfSpace x1 x2 x <= 0 && halfSpace x2 x3 x <= 0 && halfSpace x3 x1 x <=0)
 
 type Im  = I.Image I.VU I.RGB Double
 
@@ -45,8 +55,8 @@ halfSpace p1 p2 c =
 tringle =
   Triangle 
     (S.vec2 0 0)
-    (S.vec2 50 50)
-    (S.vec2 98 24)
+    (S.vec2 200 200)
+    (S.vec2 300 100)
 
 bingle = 
   Triangle
@@ -62,14 +72,54 @@ toTuple x =
   let unwrapped =  S.unwrap x
   in (L.atIndex unwrapped 0, L.atIndex unwrapped 1)
 
-rasterImage :: Im 
-rasterImage = I.makeImage (100,100) (\x -> if inTriangle tringle $ toVector x then I.PixelRGB 1.0 1.0 1.0 else I.PixelRGB 0.0 0.0 0.0)
-
 black = I.PixelRGB 0.0 0.0 0.0
 
-rasterTriangle :: Triangle -> Triangle ->  Im -> Im
-rasterTriangle so dest im = 
+rasterTriangle :: Im -> Triangle -> Triangle  -> Im
+rasterTriangle im so dest = 
   let transform = computeAffine dest so
   in I.traverse im id  (\f x -> if inTriangle dest $ toVector x then I.interpolate I.Bilinear  I.Edge (I.dims im) f $ toTuple $ toLin (transform S.#> toAffine (toVector x)) else black) 
 
-testRaster = rasterTriangle tringle bingle 
+warpTriangles :: Im -> [Triangle] -> [Triangle] -> Im 
+warpTriangles im so dest = sum $ zipWith (rasterTriangle im) so dest 
+
+triangleFromString :: [String] -> Triangle
+triangleFromString (fst:snd:thrd:frth:ffth:sxth:_) = Triangle (vfs snd fst) (vfs frth thrd) (vfs sxth ffth)
+
+csvToTriangles :: [[String]] -> [Triangle]
+csvToTriangles x = map triangleFromString (take (length x - 1) x)
+
+vfs :: String -> String -> S.R 2
+vfs a b = S.vector [read a, read b]
+
+interp :: Double -> Triangle -> Triangle -> Triangle
+interp x (Triangle a1 b1 c1) (Triangle a2 b2 c2) = Triangle (scale x a2 + scale (1-x) a1) (scale x b2 + scale (1-x) b1) (scale x c2 + scale (1-x) c1)
+
+interpTriangles :: Double -> [Triangle] -> [Triangle] -> [Triangle]
+interpTriangles x = zipWith $ interp x
+
+scale ::Double -> S.R 2 -> S.R 2
+scale a = (S.#>) $ S.matrix [a, 0, 0, a]
+
+x |*> Triangle a2 b2 c2 = Triangle (x*a2) (x*b2) (x*c2)
+
+
+(+>) :: Triangle -> Triangle -> Triangle
+Triangle a1 b1 c1 +> Triangle a2 b2 c2 = Triangle (a1 + a2) (b1 + b2) (c1 + c2)
+
+zero = Triangle 0 0 0
+
+sumTriangles :: [[Triangle]] -> [Triangle]
+sumTriangles = foldl1 $ zipWith (+>) 
+
+averageTriangles :: [[Triangle]] -> [Triangle]
+averageTriangles x = map ((1.0 / fromIntegral (length x))|*>) $ sumTriangles x 
+
+interpImages :: Double -> Im -> [Triangle] -> Im -> [Triangle] -> Im
+interpImages a im1 t1 im2 t2 = 
+  let triangleSet = interpTriangles a t1 t2
+      firstWarped = warpTriangles im1 t1 triangleSet 
+      secondWarped = warpTriangles im2 t2 triangleSet
+  in iscale (1-a) firstWarped  + iscale a secondWarped
+
+iscale :: Double -> Im -> Im
+iscale x = I.map  (\(I.PixelRGB r g b) -> I.PixelRGB (r*x) (g*x) (b*x))
